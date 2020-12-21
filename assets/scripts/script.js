a6c0b5c0511942678de45dc27e4f9ac63a7f0280
Justin Davis <jrdavis4@gmail.com>
2020-12-20 23:03:57 -0500

    added js code to create questions and answers

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,51 +1,91 @@
 //Assignments
-var startBtn = document.querySelector(".startBtn");
-var answerDiv = document.querySelector(".answers");
-var cardText = document.querySelector(".card-text");
-
+var startBtnEl = document.querySelector(".startBtn");
+// var cardBodyEl = document.querySelector(".card-body");
+var quizContainerEl = document.querySelector(".quiz-container");
+var cardTextEl = document.querySelector(".card-text");
 var questionList = [
   {
-    question: "This is questions number 1?"
-    a: "Answer 1"
-    b: "Answer 2"
-    c: "Answer 3"
-    d: "Answer 4"
+    question: "This is questions number 1?",
+    answers: ["Answer 1","Answer 2","Answer 3","Answer 4"]
   },
   {
-    question: "This is questions number 2?"
-    a: "Answer 1"
-    b: "Answer 2"
-    c: "Answer 3"
-    d: "Answer 4"
-  },
-  {
-    question: "This is questions number 3?"
-    a: "Answer 1"
-    b: "Answer 2"
-    c: "Answer 3"
-    d: "Answer 4"
+    question: "This is questions number 2?",
+    answers: ["Answer 1","Answer 2","Answer 3","Answer 4"]
   }
 ]
 
+//Create list of index numbers to randomize questions 
+var queue = [];
+for (var i = 0; i < questionList.length; i++){
+  queue.push(i);
+}
 
 function init(){
 
 
 }
 
-function startQuiz(){
+function nextQuestion(){
+
+  if (queue.length === 0){
+
+    endQuiz();
+
+  } else {
+
+    clearQuiz();
+
+    //Get random question and remove it from queue
+    var randIndex = queue[Math.floor(Math.random() * queue.length)]
+    var randQuestion = questionList[randIndex];
+    queue.splice(queue.indexOf(randIndex), 1);
+
+    //Create question element and append
+    var questionEl = document.createElement("h4");
+    questionEl.innerHTML = questionList[randIndex].question;
+    quizContainerEl.appendChild(questionEl);
+
+    //Loop through all answers to create and append
+    for (var i = 0; i < questionList[randIndex].answers.length; i++){
+      var answerEl = document.createElement("button");
+      answerEl.setAttribute("class", "answerBtn");
+      answerEl.innerHTML = questionList[randIndex].answers[i];
+      quizContainerEl.appendChild(answerEl);
+    }
+  }
+}
+
+
+function clearQuiz(){
+
+  while (quizContainerEl.firstChild){
+    quizContainerEl.removeChild(quizContainerEl.firstChild);
+  }
+}
+
+
+function endQuiz(){
+
 
 
 }
+
 
 function storeGrade(){
 
 
+
 }
 
 
-startBtn.addEventListener("click", function(e){
-  cardText.style.display = "none";
-  startBtn.style.display = "none";
-  startQuiz();
+quizContainerEl.addEventListener("click", function(e){
+  if (e.target.matches("button")){
+    nextQuestion();
+  }
+})
+
+startBtnEl.addEventListener("click", function (e) {
+  cardTextEl.style.display = "none";
+  startBtnEl.style.display = "none";
+  nextQuestion();
 })
