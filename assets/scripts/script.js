57a6b6269076432b25562aed91be02dff7b808e9
Justin Davis <jrdavis4@gmail.com>
2020-12-21 23:35:50 -0500

    added some scoring code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,14 +3,25 @@ var startBtnEl = document.querySelector(".startBtn");
 // var cardBodyEl = document.querySelector(".card-body");
 var quizContainerEl = document.querySelector(".quiz-container");
 var cardTextEl = document.querySelector(".card-text");
+var currentScore = 0;
+var highScores = [
+  {
+    name: "Justin",
+    score: 80
+  },{
+    name: "Adam",
+    score: 90
+  }
+]
 var questionList = [
   {
-    question: "This is questions number 1?",
-    answers: ["Answer 1","Answer 2","Answer 3","Answer 4"]
-  },
-  {
-    question: "This is questions number 2?",
-    answers: ["Answer 1","Answer 2","Answer 3","Answer 4"]
+    question: "This is question number 1?",
+    answers: ["Answer 1","Answer 2","Answer 3","Answer 4"],
+    correctIndex: 0
+  },{
+    question: "This is question number 2?",
+    answers: ["Answer 1","Answer 2","Answer 3","Answer 4","Answer 5"],
+    correctIndex: 3
   }
 ]
 
@@ -22,6 +33,11 @@ for (var i = 0; i < questionList.length; i++){
 
 function init(){
 
+  if (!localStorage.getItem("highScores")){
+    return;
+  } else {
+    highScores = JSON.parse(localStorage.getItem("highScores"));
+  }
 
 }
 
@@ -42,6 +58,7 @@ function nextQuestion(){
 
     //Create question element and append
     var questionEl = document.createElement("h4");
+    questionEl.setAttribute("class", "question");
     questionEl.innerHTML = questionList[randIndex].question;
     quizContainerEl.appendChild(questionEl);
 
@@ -49,6 +66,14 @@ function nextQuestion(){
     for (var i = 0; i < questionList[randIndex].answers.length; i++){
       var answerEl = document.createElement("button");
       answerEl.setAttribute("class", "answerBtn");
+
+      //Set data-correct to true if index matches the correct answer
+      if (randQuestion.correctIndex === i){
+        answerEl.setAttribute("data-correct", true);
+      } else {
+        answerEl.setAttribute("data-correct", false);
+      }
+
       answerEl.innerHTML = questionList[randIndex].answers[i];
       quizContainerEl.appendChild(answerEl);
     }
@@ -66,7 +91,7 @@ function clearQuiz(){
 
 function endQuiz(){
 
-
+  console.log(currentScore);
 
 }
 
@@ -79,8 +104,15 @@ function storeGrade(){
 
 
 quizContainerEl.addEventListener("click", function(e){
+
   if (e.target.matches("button")){
+
+    if (e.target.getAttribute("data-correct") === "true"){
+      currentScore += 10;
+    }
+
     nextQuestion();
+    
   }
 })
 
