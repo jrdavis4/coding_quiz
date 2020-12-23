5caa5e7f470b420fce53c30d62f2fc811e52334b
Justin Davis <jrdavis4@gmail.com>
2020-12-22 23:26:05 -0500

    added background img and more scoring logic

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,23 +1,19 @@
 //Assignments
 var startBtnEl = document.querySelector(".startBtn");
-// var cardBodyEl = document.querySelector(".card-body");
 var quizContainerEl = document.querySelector(".quiz-container");
 var cardTextEl = document.querySelector(".card-text");
 var currentScore = 0;
 var highScores = [
-  {
-    name: "Justin",
-    score: 80
-  },{
-    name: "Adam",
-    score: 90
-  }
+  {name: "Justin", score: 80},
+  {name: "Adam", score: 90},
+  {name: "Ashley", score: 85}
 ]
+
 var questionList = [
   {
-    question: "This is question number 1?",
-    answers: ["Answer 1","Answer 2","Answer 3","Answer 4"],
-    correctIndex: 0
+    question: "Which character is used for variable assignment?",
+    answers: ["===","=","<>","=="],
+    correctIndex: 1
   },{
     question: "This is question number 2?",
     answers: ["Answer 1","Answer 2","Answer 3","Answer 4","Answer 5"],
@@ -25,7 +21,7 @@ var questionList = [
   }
 ]
 
-//Create list of index numbers to randomize questions 
+//Create list of index numbers to pull from randomly (later) 
 var queue = [];
 for (var i = 0; i < questionList.length; i++){
   queue.push(i);
@@ -34,6 +30,7 @@ for (var i = 0; i < questionList.length; i++){
 function init(){
 
   if (!localStorage.getItem("highScores")){
+    console.log("yes");
     return;
   } else {
     highScores = JSON.parse(localStorage.getItem("highScores"));
@@ -43,46 +40,41 @@ function init(){
 
 function nextQuestion(){
 
-  if (queue.length === 0){
+  clearQuiz();
 
-    endQuiz();
+  //Get random question and remove it from queue
+  var randIndex = queue[Math.floor(Math.random() * queue.length)]
+  var randQuestion = questionList[randIndex];
+  queue.splice(queue.indexOf(randIndex), 1);
 
-  } else {
+  //Create question element and append
+  var questionEl = document.createElement("h4");
+  questionEl.setAttribute("class", "question");
+  questionEl.innerHTML = questionList[randIndex].question;
+  quizContainerEl.appendChild(questionEl);
 
-    clearQuiz();
+  //Loop through all answers to create and append
+  for (var i = 0; i < questionList[randIndex].answers.length; i++){
+    var answerEl = document.createElement("button");
+    answerEl.setAttribute("class", "answerBtn");
 
-    //Get random question and remove it from queue
-    var randIndex = queue[Math.floor(Math.random() * queue.length)]
-    var randQuestion = questionList[randIndex];
-    queue.splice(queue.indexOf(randIndex), 1);
-
-    //Create question element and append
-    var questionEl = document.createElement("h4");
-    questionEl.setAttribute("class", "question");
-    questionEl.innerHTML = questionList[randIndex].question;
-    quizContainerEl.appendChild(questionEl);
-
-    //Loop through all answers to create and append
-    for (var i = 0; i < questionList[randIndex].answers.length; i++){
-      var answerEl = document.createElement("button");
-      answerEl.setAttribute("class", "answerBtn");
-
-      //Set data-correct to true if index matches the correct answer
-      if (randQuestion.correctIndex === i){
-        answerEl.setAttribute("data-correct", true);
-      } else {
-        answerEl.setAttribute("data-correct", false);
-      }
-
-      answerEl.innerHTML = questionList[randIndex].answers[i];
-      quizContainerEl.appendChild(answerEl);
+    //Set data-correct to true if index matches the correct answer
+    if (randQuestion.correctIndex === i){
+      answerEl.setAttribute("data-correct", true);
+    } else {
+      answerEl.setAttribute("data-correct", false);
     }
+
+    answerEl.innerHTML = questionList[randIndex].answers[i];
+    quizContainerEl.appendChild(answerEl);
+
   }
 }
 
 
 function clearQuiz(){
 
+  //Clear quiz area
   while (quizContainerEl.firstChild){
     quizContainerEl.removeChild(quizContainerEl.firstChild);
   }
@@ -91,33 +83,39 @@ function clearQuiz(){
 
 function endQuiz(){
 
-  console.log(currentScore);
+  //Get initials for high score
+  var x = "test"
 
-}
+  //Insert into the highScores array, sort based on score, and remove the lowest
+  highScores.push({name: x, score: currentScore});
+  highScores.sort(function(a,b){
+    return (b.score - a.score);
+  })
+  highScores.splice(highScores.length - 1, 1);
 
-
-function storeGrade(){
-
-
+  //Store highScores into local storage
+  localStorage.setItem("highScores", JSON.stringify(highScores));
 
 }
 
 
 quizContainerEl.addEventListener("click", function(e){
-
   if (e.target.matches("button")){
-
     if (e.target.getAttribute("data-correct") === "true"){
-      currentScore += 10;
+      currentScore += 100;
     }
 
-    nextQuestion();
-    
+    if (queue.length === 0){
+      endQuiz();
+    } else {
+      nextQuestion();
+    }
   }
 })
 
 startBtnEl.addEventListener("click", function (e) {
   cardTextEl.style.display = "none";
   startBtnEl.style.display = "none";
+  init();
   nextQuestion();
 })
